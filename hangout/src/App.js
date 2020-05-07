6bd4d2ca1d11b8397c4674a207606e5ac5e4e82b
Sam Farley <farleysm8909@uwec.edu>
2020-05-07 16:25:39 -0500

    added react app files, started login page

---
+++ hangout/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import {Login} from './login.js';
+
+function App() {
+  return (
+    <div id="hangout-root">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
