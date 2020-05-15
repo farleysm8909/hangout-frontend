d4a767ac9c359ea0b89c37536a6b584aaed7f405
Sam Farley <farleysm8909@uwec.edu>
2020-05-15 12:46:08 -0500

    finished updating code through redux3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {Event} from './event.js';
 import {Login} from './login.js';
 import {useSelector, useDispatch} from 'react-redux';
-import {loadEvents} from './actions';
+import {loadMonth} from './actions';
 
 function App() {
 
@@ -11,11 +11,7 @@ function App() {
   const dispatch = useDispatch(); //gets handle on dispatch fxn
 
   useEffect(() => {
-    dispatch(loadEvents([
-      {id: 1, event_name: "Backyard BBQ", month: 7, day: 23, year: 2020, start_time: "4pm", end_time: "8pm"},
-      {id: 2, event_name: "Diane's birthday", month: 7, day: 11, year: 2020, start_time: "6pm", end_time: "10pm"},
-      {id: 3, event_name: "School shopping", month: 8, day: 29, year: 2020, start_time: "1pm", end_time: "3:30pm"}
-    ]));
+    dispatch(loadMonth(7, 2020));
 }, [dispatch]); //no real dependencies so events only run once when app renders (redux2)
 
   return (
--- src/actions.js
+++ src/actions.js
@@ -8,3 +8,28 @@ export function loadEvents(events) {
         payload: events,
     };
 }
+
+function checkForErrors(response) {
+    if (!response.ok) {
+        throw Error(`${response.status}: ${response.statusText}`);
+    }
+    return response;
+}
+
+const host = 'https://hangout-api.duckdns.org:8442';
+
+//this function does not return an action object (with keys type and payload) like a true action creator
+//it returns a fxn, and in order to allow this, we have to install thunk plugin
+export function loadMonth(month, year) {
+    return dispatch => {
+        fetch(`${host}/hangout/${month}/${year}`)
+            .then(checkForErrors)
+            .then(response => response.json())
+            .then(data => {
+                if (data.ok) {
+                    dispatch(loadEvents(data.hangout));
+                }
+            })
+            .catch(e => console.error(e));
+    };
+}
