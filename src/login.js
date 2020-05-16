71f30509077e68016b09171ed4e2c58901efc900
Sam Farley <farleysm8909@uwec.edu>
2020-05-16 13:49:45 -0500

    improved login UI added create new account feature

--- src/login.js
+++ src/login.js
@@ -10,10 +10,25 @@ export function Login() {
                 <div className="login-title">Login</div>
                 <div className="login-info">
                     <label for="username">Username:</label>
-                    <input type="text" className="username"></input>
+                    <input type="text" className="user-input"></input>
                     <label for="password">Password:</label>
-                    <input type="text" className="password"></input>
+                    <input type="text" className="user-input"></input>
                     <button type="submit" className="submit-button" value="login" /*onClick={() => Login.remove(login-container)}*/>Login</button>
+                </div>
+            </div>
+            <div className="vLine"></div>
+            <div className="account-container">
+                <div className="account-title">Create New Account</div>
+                <div className="login-info">
+                        <label for="firstname">First Name:</label>
+                        <input type="text" className="user-input"></input>
+                        <label for="lastname">Last Name:</label>
+                        <input type="text" className="user-input"></input>
+                        <label for="username">Username:</label>
+                        <input type="text" className="user-input"></input>
+                        <label for="password">Password:</label>
+                        <input type="text" className="user-input"></input>
+                    <button type="submit" className="submit-button" value="Create New Account" /*onClick={() => Login.remove(login-container)}*/>Create Account</button>
                 </div>
             </div>
         </div>
